c6ab953bc53ba4267a3dc6c78740a9372e812328
Shibaan Askari <askarishibaan1@gmail.com>
2026-07-27 15:21:06 +0530

    feat: add song item HTML structure

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const SongItem = () => {
   return (
     <div>
-      
+
+      <img src="" alt="" />
+
+      <div>
+        <p>title</p>
+        <p>artist</p>
+      </div>
+
+      <div>
+        <button>play</button>
+        <button>⋮ menu</button>
+
+        <div className='dropdown-menu'>
+          <button>Delete</button>
+          <button>play next</button>
+          <button>move to top</button>
+
+        </div>
+      </div>
     </div>
   )
 }
