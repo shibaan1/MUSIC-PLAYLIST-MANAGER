91a9dc9e551c58043cde4a9ab58cae7f90af0fad
Shibaan Askari <askarishibaan1@gmail.com>
2026-07-27 15:11:13 +0530

    feat: add song form modal HTML structure

--- src/components/SongForm.jsx
+++ src/components/SongForm.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const SongForm = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='modal-overlay'>
+        <div className='modal-box'>
+          <button>close/cancel</button>
+          <input type="text" placeholder='Song-Name' />
+          <input type="text" placeholder='Artist Name' />
+          <input type="url" placeholder='enter url' />
+          <button>Add Song</button>
+        </div>
+      </div>
+    </>
   )
 }
 
