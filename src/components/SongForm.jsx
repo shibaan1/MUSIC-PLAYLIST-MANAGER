9565b8b3568a5e9be2e8564cc5b043ffdc462341
Shibaan Askari <askarishibaan1@gmail.com>
2026-07-28 14:02:56 +0530

    feat: add song from with validation and dispatch

--- src/components/SongForm.jsx
+++ src/components/SongForm.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { PlaylistContext } from '../context/playlistContext'
 
-const SongForm = ({onClose}) => {
+const SongForm = ({ onClose }) => {
+
+  const [title, setTitle] = useState('')
+  const [artist, setArtist] = useState('')
+  const [url, setUrl] = useState('')
+  const { state, dispatch } = useContext(PlaylistContext)
+
+  function handleSubmit(e) {
+
+    e.preventDefault()
+
+    if (!title || !artist || !url) {
+      alert('please fill all the fields')
+      return
+    }
+
+    dispatch({ type: 'ADD_SONG', payload: { id: Date.now(), title: title, artist: artist, url: url } })
+
+    onClose()
+  }
+
   return (
     <>
       <div className='modal-overlay'>
         <div className='modal-box'>
           <button onClick={onClose}>close/cancel</button>
-          <input type="text" placeholder='Song-Name' />
-          <input type="text" placeholder='Artist Name' />
-          <input type="url" placeholder='enter url' />
-          <button>Add Song</button>
+          <input type="text" placeholder='Song-Name' value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input type="text" placeholder='Artist Name' value={artist} onChange={(e) => setArtist(e.target.value)} />
+          <input type="url" placeholder='enter url' value={url} onChange={(e) => setUrl(e.target.value)} />
+
+          <button onClick={handleSubmit}>Add Song</button>
+
         </div>
       </div>
     </>
