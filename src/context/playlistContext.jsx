50a3b52e9d3de25e2063b5726045dd1721be40f9
Shibaan Askari <askarishibaan1@gmail.com>
2026-07-29 15:47:47 +0530

    feat: add all reducer actions

--- src/context/playlistContext.jsx
+++ src/context/playlistContext.jsx
@@ -20,10 +20,70 @@ function PlaylistReducer(state, action) {
         case "CLEAR_PLAYLIST":
             return { ...state, songs: [] }
 
+        case "PLAY_SONG":
+            return { ...state, currentSongId: action.payload, isPlaying: true }
+
+        case 'PAUSE_SONG':
+            return { ...state, isPlaying: false }
+
+        case 'TOGGLE_SHUFFLE':
+            return { ...state, shuffle: !state.shuffle }
+
+        case 'MOVE_UP':
+{
+            const sing = [...state.songs]
+            const index = state.songs.findIndex(song => song.id === action.payload)
+
+            if (index === 0) {
+                return state
+            }
+
+            const temp = sing[index]
+            sing[index] = sing[index - 1]
+            sing[index - 1] = temp
+
+            return { ...state, songs: sing }}
+
+        case 'MOVE_DOWN':
+{
+            const songs = [...state.songs]
+            const index = state.songs.findIndex(song => song.id === action.payload)
+
+            if (index === songs.length - 1) {
+                return state
+            }
+
+            const temp = songs[index]
+            songs[index] = songs[index + 1]
+            songs[index + 1] = temp
+
+            return { ...state, songs }}
+
+        case 'MOVE_TO_TOP':
+{
+            const songs = [...state.songs]
+            const currsong = songs.find(song => song.id === action.payload)
+            const remsongs = songs.filter(song => song.id !== action.payload)
+
+            return { ...state, songs: [currsong, ...remsongs] }
+}
+        case 'PLAY_NEXT':{
+
+            const songs2 = [...state.songs]
+
+            const currpos = songs2.findIndex(song => song.id === state.currentSongId)
+            const remind = songs2.findIndex(song => song.id === action.payload)
+
+            const [taregtSong] = songs2.splice(remind, 1)
+            songs2.splice(currpos + 1, 0, taregtSong)
+
+            return { ...state, songs: songs2 }}
+
         default:
             return state
     }
 }
+
 
 export const PlaylistContext = createContext()
 
