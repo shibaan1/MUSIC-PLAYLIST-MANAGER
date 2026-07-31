87072dd71cb6de933de7b8c812ac46a1bac5ad1c
Shibaan Askari <askarishibaan1@gmail.com>
2026-07-31 16:09:03 +0530

    feat: add audio playback with play/pause toggle

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -3,7 +3,17 @@ import { PlaylistContext } from '../context/playlistContext'
 
 const SongItem = ({ id, title, artist, url }) => {
 
-  const { dispatch } = useContext(PlaylistContext)
+  const { state, dispatch } = useContext(PlaylistContext)
+
+  function handleClick() {
+    if (state.currentSongId === id && state.isPlaying === true) {
+      dispatch({ type: 'PAUSE_SONG' })
+    }
+
+    else {
+      dispatch({ type: 'PLAY_SONG', payload: id })
+    }
+  }
   return (
     <div>
 
@@ -15,13 +25,13 @@ const SongItem = ({ id, title, artist, url }) => {
       </div>
 
       <div>
-        <button>play</button>
+        <button onClick={handleClick}>play</button>
         <button>⋮ menu</button>
 
         <div className='dropdown-menu'>
           <button onClick={() => dispatch({ type: 'DELETE_SONG', payload: id })}>Delete</button>
-          <button onClick={() => dispatch({type: 'PLAY_NEXT' , payload:id})}>play next</button>
-          <button onClick={() => dispatch({type: 'MOVE_TO_TOP' , payload: id})}>move to top</button>
+          <button onClick={() => dispatch({ type: 'PLAY_NEXT', payload: id })}>play next</button>
+          <button onClick={() => dispatch({ type: 'MOVE_TO_TOP', payload: id })}>move to top</button>
 
         </div>
       </div>
--- src/context/playlistContext.jsx
+++ src/context/playlistContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useEffect, useReducer, useRef } from 'react'
 
 const initialState = {
     songs: [],
@@ -30,44 +30,46 @@ function PlaylistReducer(state, action) {
             return { ...state, shuffle: !state.shuffle }
 
         case 'MOVE_UP':
-{
-            const sing = [...state.songs]
-            const index = state.songs.findIndex(song => song.id === action.payload)
+            {
+                const sing = [...state.songs]
+                const index = state.songs.findIndex(song => song.id === action.payload)
 
-            if (index === 0) {
-                return state
+                if (index === 0) {
+                    return state
+                }
+
+                const temp = sing[index]
+                sing[index] = sing[index - 1]
+                sing[index - 1] = temp
+
+                return { ...state, songs: sing }
             }
-
-            const temp = sing[index]
-            sing[index] = sing[index - 1]
-            sing[index - 1] = temp
-
-            return { ...state, songs: sing }}
 
         case 'MOVE_DOWN':
-{
-            const songs = [...state.songs]
-            const index = state.songs.findIndex(song => song.id === action.payload)
+            {
+                const songs = [...state.songs]
+                const index = state.songs.findIndex(song => song.id === action.payload)
 
-            if (index === songs.length - 1) {
-                return state
+                if (index === songs.length - 1) {
+                    return state
+                }
+
+                const temp = songs[index]
+                songs[index] = songs[index + 1]
+                songs[index + 1] = temp
+
+                return { ...state, songs }
             }
 
-            const temp = songs[index]
-            songs[index] = songs[index + 1]
-            songs[index + 1] = temp
-
-            return { ...state, songs }}
-
         case 'MOVE_TO_TOP':
-{
-            const songs = [...state.songs]
-            const currsong = songs.find(song => song.id === action.payload)
-            const remsongs = songs.filter(song => song.id !== action.payload)
+            {
+                const songs = [...state.songs]
+                const currsong = songs.find(song => song.id === action.payload)
+                const remsongs = songs.filter(song => song.id !== action.payload)
 
-            return { ...state, songs: [currsong, ...remsongs] }
-}
-        case 'PLAY_NEXT':{
+                return { ...state, songs: [currsong, ...remsongs] }
+            }
+        case 'PLAY_NEXT': {
 
             if (state.currentSongId === null) return state
 
@@ -79,19 +81,52 @@ function PlaylistReducer(state, action) {
             const [taregtSong] = songs2.splice(remind, 1)
             songs2.splice(currpos + 1, 0, taregtSong)
 
-            return { ...state, songs: songs2 }}
+            return { ...state, songs: songs2 }
+        }
 
         default:
             return state
     }
 }
 
-
 export const PlaylistContext = createContext()
 
 const PlaylistProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(PlaylistReducer, initialState)
+    const audioRef = useRef(null)
+
+    useEffect(() => {
+
+        if (state.currentSongId === null) { return }
+        const currSong = state.songs.find((song) => song.id === state.currentSongId)
+
+        audioRef.current = new Audio(currSong.url)
+
+        return () => {
+            audioRef.current.pause()
+        }
+
+    }, [state.currentSongId])
+
+
+    useEffect(() => {
+
+        if (!audioRef.current) { return }
+
+        if (state.isPlaying) {
+            audioRef.current.play()
+
+        }
+        else {
+            audioRef.current.pause()
+        }
+
+        return () => {
+            audioRef.current.pause()
+        }
+    }, [state.isPlaying])
+
     return (
 
         <PlaylistContext.Provider value={{ state, dispatch }}>
